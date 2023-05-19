4bbb53e1fb4aa50ff7438487a630228b963ed674
petro_thomas <peter88tom@gmail.com>
2023-05-19 14:10:45 +0200

    Teach robot to fill in the sales form

--- task.py
+++ task.py
@@ -1,5 +1,6 @@
 from RPA.Browser.Selenium import Selenium
 from RPA.HTTP import HTTP
+
 import time
 
 browser = Selenium()
@@ -33,11 +34,30 @@ def download_the_excel_file():
 
 
 def fill_and_submit_the_form_for_one_person():
-    pass
+    """ Fill in the sales form """
+    # browser.wait_until_page_contains("css:sales-form", timeout=10)
+    time.sleep(5)
+
+    first_name = browser.find_element("//input[@name='firstname']")
+    browser.input_text(first_name, "John")
+
+    last_name = browser.find_element("//input[@name='lastname']")
+    browser.input_text(last_name, "Doe")
+
+    sales_target = browser.find_element("//select[@id='salestarget']")
+    browser.select_from_list_by_value(sales_target, "55000")
+
+    sales_result = browser.find_element("//input[@name='salesresult']")
+    browser.input_text(sales_result, 10000)
+
+    submit_sale = browser.find_element("//button[@type='submit']")
+    browser.click_button(submit_sale)
+
+    time.sleep(60)
 
 
 def fill_the_form_using_the_data_from_the_excel_file():
-    pass
+    fill_and_submit_the_form_for_one_person()
 
 
 def collect_the_results():
