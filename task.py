446d3144ec36c19721e521015fc4ccd6d4220e0c
petro_thomas <peter88tom@gmail.com>
2023-05-19 14:34:27 +0200

    Teach robot to fill in the sales form using data from the excel file

--- task.py
+++ task.py
@@ -1,7 +1,9 @@
 from RPA.Browser.Selenium import Selenium
 from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
 
 import time
+import os
 
 browser = Selenium()
 url = "https://robotsparebinindustries.com"
@@ -33,31 +35,40 @@ def download_the_excel_file():
         url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
 
 
-def fill_and_submit_the_form_for_one_person():
+def fill_and_submit_the_form_for_one_person(sales_person):
     """ Fill in the sales form """
     # browser.wait_until_page_contains("css:sales-form", timeout=10)
-    time.sleep(5)
+
+    print(sales_person["First Name"])
 
     first_name = browser.find_element("//input[@name='firstname']")
-    browser.input_text(first_name, "John")
+    browser.input_text(first_name, sales_person["First Name"])
 
     last_name = browser.find_element("//input[@name='lastname']")
-    browser.input_text(last_name, "Doe")
+    browser.input_text(last_name, sales_person["Last Name"])
 
     sales_target = browser.find_element("//select[@id='salestarget']")
-    browser.select_from_list_by_value(sales_target, "55000")
+    browser.select_from_list_by_value(
+        sales_target, f"{sales_person['Sales Target']}")
 
     sales_result = browser.find_element("//input[@name='salesresult']")
-    browser.input_text(sales_result, 10000)
+    browser.input_text(sales_result, sales_person["Sales"])
 
     submit_sale = browser.find_element("//button[@type='submit']")
     browser.click_button(submit_sale)
 
-    time.sleep(60)
+    # time.sleep(30)
 
 
 def fill_the_form_using_the_data_from_the_excel_file():
-    fill_and_submit_the_form_for_one_person()
+    time.sleep(5)
+    excel = Files()
+    excel.open_workbook("SalesData.xlsx")
+    sales_persons = excel.read_worksheet_as_table(header=True)
+    excel.close_workbook()
+
+    for sales_person in sales_persons:
+        fill_and_submit_the_form_for_one_person(sales_person)
 
 
 def collect_the_results():
