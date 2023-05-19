a7d1b27d0edff25cafaf147bd6a7a511a3ff30c6
petro_thomas <peter88tom@gmail.com>
2023-05-19 15:41:04 +0200

    Teach robot to take screenshot of a div

--- task.py
+++ task.py
@@ -37,10 +37,6 @@ def download_the_excel_file():
 
 def fill_and_submit_the_form_for_one_person(sales_person):
     """ Fill in the sales form """
-    # browser.wait_until_page_contains("css:sales-form", timeout=10)
-
-    print(sales_person["First Name"])
-
     first_name = browser.find_element("//input[@name='firstname']")
     browser.input_text(first_name, sales_person["First Name"])
 
@@ -57,10 +53,9 @@ def fill_and_submit_the_form_for_one_person(sales_person):
     submit_sale = browser.find_element("//button[@type='submit']")
     browser.click_button(submit_sale)
 
-    # time.sleep(30)
-
 
 def fill_the_form_using_the_data_from_the_excel_file():
+    """ Read data from excel file and fill in the sales form """
     time.sleep(5)
     excel = Files()
     excel.open_workbook("SalesData.xlsx")
@@ -72,7 +67,10 @@ def fill_the_form_using_the_data_from_the_excel_file():
 
 
 def collect_the_results():
-    pass
+    """ Take a screenshot of the div that contain sales result summary """
+    time.sleep(5)
+    sales_summary_div = browser.find_element("css:div.sales-summary")
+    browser.screenshot(sales_summary_div, filename="output/sales_summary.png")
 
 
 def export_the_table_as_pdf():
